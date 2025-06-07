d89a37f17fb11f8a7b8b3c8b314fa4219cddf888
kimyonghui <kyhui1115@gmail.com>
2025-06-07 15:56:54 +0900

    init. 초기설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  devIndicators: {
+    buildActivity: false,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
