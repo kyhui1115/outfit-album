ad59e3ea8ad3db68fb98c8745e1071fa90505632
kimyonghui <kyhui1115@gmail.com>
2025-06-08 16:28:03 +0900

    feat. 로그인 페이지로 리다이렉트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
+import { redirect } from "next/navigation";
+
+export default function Page() {
+  redirect("/auth");
+
+  // 인증 여부에 따라 페이지 리다이렉트
   return <></>;
 }
