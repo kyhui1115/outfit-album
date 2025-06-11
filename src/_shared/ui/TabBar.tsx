c0067c0d41fa59f13c16a76a3e7d497a75928766
kimyonghui <kyhui1115@gmail.com>
2025-06-11 21:30:20 +0900

    feat. TabBar 컴포넌트에 탭 바 목록 추가

--- src/_shared/ui/TabBar.tsx
+++ src/_shared/ui/TabBar.tsx
@@ -1,5 +1,17 @@
+import TabBarItem from "@/_features/tabBar/TabBarItem";
+import DIC from "@/_shared/dictionary";
+
 export default function TabBar() {
   return (
-    <div className="border-darkgray fixed bottom-0 h-[50px] w-full max-w-[1024px] border-t"></div>
+    <div className="border-darkgray fixed bottom-0 flex h-[50px] w-full max-w-[1024px] border-t">
+      {DIC.tabBarList.map((tabBar, idx) => (
+        <TabBarItem
+          key={tabBar.name}
+          icon={tabBar.icon}
+          url={tabBar.url}
+          idx={idx}
+        />
+      ))}
+    </div>
   );
 }
