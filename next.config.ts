28985309e1cc6d2fc5f74b57a224d8fd44cc91ef
kimyonghui <kyhui1115@gmail.com>
2025-06-15 12:23:54 +0900

    feat. PWA 설정

--- next.config.ts
+++ next.config.ts
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: false,
 };
 
-module.exports = nextConfig;
+const withPWA = require("next-pwa")({
+  dest: "public",
+  // disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = withPWA(nextConfig);
