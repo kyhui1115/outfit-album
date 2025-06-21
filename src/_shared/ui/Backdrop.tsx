72e2134d9c45103a306a38d2fb5f9ee1dbd47213
kimyonghui <kyhui1115@gmail.com>
2025-06-21 15:57:26 +0900

    fix. 잘못된 스토어 set함수 수정

--- src/_shared/ui/Backdrop.tsx
+++ src/_shared/ui/Backdrop.tsx
@@ -3,12 +3,17 @@
 import useIsModal from "../store/isModal";
 
 export default function BackDrop() {
-  const { setIsModal } = useIsModal();
+  const { setIsCalendarDayModal, setIsPictureSelectModal } = useIsModal();
+
+  const buttonhandler = () => {
+    setIsCalendarDayModal(false);
+    setIsPictureSelectModal(false);
+  };
 
   return (
     <button
       className="bg-darkgray fixed top-0 left-0 z-10 h-full w-full opacity-70"
-      onClick={() => setIsModal(false)}
+      onClick={buttonhandler}
     />
   );
 }
