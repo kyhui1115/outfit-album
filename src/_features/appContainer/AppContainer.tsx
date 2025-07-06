9c262b74ffacbefd267b0166bd85a7631dcda376
kimyonghui <kyhui1115@gmail.com>
2025-07-06 19:31:18 +0900

    feat. useImageUrls 임포트 경로 수정 및 Header 컴포넌트 주석 삭제

--- src/_features/appContainer/AppContainer.tsx
+++ src/_features/appContainer/AppContainer.tsx
@@ -2,8 +2,8 @@
 
 import { ReactNode } from "react";
 
+import useImageUrls from "@/_shared/store/Image";
 import useIsModal from "@/_shared/store/isModal";
-import useImageUrls from "@/_shared/store/previewImageUrl";
 import Header from "@/_shared/ui/Header";
 
 import Modal from "../../_shared/ui/modal/Modal";
@@ -32,14 +32,13 @@ export default function AppContainer({ children }: Props) {
   };
 
   return (
-    <div className="relative flex h-[100dvh] w-screen max-w-[600px] flex-col items-center justify-center bg-white">
+    <div className="border-border-default relative flex h-[100dvh] w-screen max-w-[600px] flex-col items-center justify-center bg-white">
       <Modal open={isCalendarDayModal} onClose={calendarCloseHandler}>
         {<CalendarDayModal />}
       </Modal>
       <Modal open={isAlbumModal} onClose={albumCloseHandler}>
         {<AlbumModal />}
       </Modal>
-      <Header name="archive:" />
       <div className="h-full w-full">{children}</div>
     </div>
   );
