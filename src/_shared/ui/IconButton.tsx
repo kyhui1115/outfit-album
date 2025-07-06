214554e96f3f7dac5716a4a093cacf2d6384e2e5
kimyonghui <kyhui1115@gmail.com>
2025-07-06 14:50:00 +0900

    feat. IconButton 컴포넌트 추가

---
+++ src/_shared/ui/IconButton.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useIsMobile } from "@/_shared/hooks/useIsMobile";
+import Button from "@/_shared/ui/Button";
+
+interface Props {
+  children: React.ReactNode;
+  onClick: () => void;
+  className?: string;
+}
+
+export default function IconButton({ children, onClick, className }: Props) {
+  const isMobile = useIsMobile();
+
+  return (
+    <Button
+      onClick={onClick}
+      className={`${isMobile ? "text-lg" : "text-xl"} ${className}`}
+    >
+      {children}
+    </Button>
+  );
+}
