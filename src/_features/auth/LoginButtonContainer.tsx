e4cc72c780751831c8bd226e83af2baef901dff1
kimyonghui <kyhui1115@gmail.com>
2025-07-06 14:48:32 +0900

    style. 마진값 수정

--- src/_features/auth/LoginButtonContainer.tsx
+++ src/_features/auth/LoginButtonContainer.tsx
@@ -3,7 +3,7 @@ import NaverLoginButton from "./NaverLoginButton";
 
 export default function LoginButtonContainer() {
   return (
-    <div className="mt-auto mb-20 flex w-full max-w-[518px] flex-col gap-3">
+    <div className="mt-auto mb-10 flex w-full max-w-[518px] flex-col gap-3">
       <KakaoLoginButton />
       <NaverLoginButton />
     </div>
