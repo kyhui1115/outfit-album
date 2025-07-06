45e8211abb2913b76722036c4dff1ed3879215b6
kimyonghui <kyhui1115@gmail.com>
2025-07-06 19:32:12 +0900

    feat. Layout 컴포넌트에 Header 추가

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,4 +1,5 @@
 import TabBarList from "@/_features/tabBar/TabBarList";
+import Header from "@/_shared/ui/Header";
 import CalendarDayModalContainer from "@/_shared/ui/modal/Modal";
 
 interface Props {
@@ -8,6 +9,7 @@ interface Props {
 export default function Layout({ children }: Props) {
   return (
     <div className="relative flex h-full w-full flex-col items-center justify-center">
+      <Header name="archive:" />
       <div className="flex h-full w-full justify-center overflow-x-hidden">
         {children}
       </div>
