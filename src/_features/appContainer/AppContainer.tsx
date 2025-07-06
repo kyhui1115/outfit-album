fc6ffdaf5d0085159c89d119856c35b1e0b8fc76
kimyonghui <kyhui1115@gmail.com>
2025-07-06 14:48:17 +0900

    chore. 레이아웃 수정

--- src/_features/appContainer/AppContainer.tsx
+++ src/_features/appContainer/AppContainer.tsx
@@ -1,18 +1,45 @@
+"use client";
+
 import { ReactNode } from "react";
 
+import useIsModal from "@/_shared/store/isModal";
+import useImageUrls from "@/_shared/store/previewImageUrl";
 import Header from "@/_shared/ui/Header";
 
-import CalendarDayModalContainer from "../calendarDayModal/CalendarDayModalContainer";
+import Modal from "../../_shared/ui/modal/Modal";
+import AlbumModal from "../albumModal/AlbumModal";
+import CalendarDayModal from "../calendarDayModal/CalendarDayModal";
 
 interface Props {
   children: ReactNode;
 }
 
 export default function AppContainer({ children }: Props) {
+  const {
+    isCalendarDayModal,
+    isAlbumModal,
+    reset: modalReset,
+    setIsAlbumModal,
+  } = useIsModal();
+  const { reset: imageReset } = useImageUrls();
+  const calendarCloseHandler = () => {
+    modalReset();
+    imageReset();
+  };
+
+  const albumCloseHandler = () => {
+    setIsAlbumModal(false);
+  };
+
   return (
-    <div className="relative flex h-[100dvh] w-screen max-w-[760px] flex-col items-center justify-center bg-white">
-      <CalendarDayModalContainer />
-      <Header name="OUTFIT" />
+    <div className="relative flex h-[100dvh] w-screen max-w-[600px] flex-col items-center justify-center bg-white">
+      <Modal open={isCalendarDayModal} onClose={calendarCloseHandler}>
+        {<CalendarDayModal />}
+      </Modal>
+      <Modal open={isAlbumModal} onClose={albumCloseHandler}>
+        {<AlbumModal />}
+      </Modal>
+      <Header name="archive:" />
       <div className="h-full w-full">{children}</div>
     </div>
   );
