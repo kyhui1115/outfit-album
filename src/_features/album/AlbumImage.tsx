1bed63e16150d98e90eeba38f4e78d2fa24e2bfc
kimyonghui <kyhui1115@gmail.com>
2025-07-06 19:31:10 +0900

    feat. AlbumImage 컴포넌트에 id 속성 추가 및 버튼 클릭 핸들러 구현

--- src/_features/album/AlbumImage.tsx
+++ src/_features/album/AlbumImage.tsx
@@ -1,27 +1,32 @@
+"use client";
+
 import Image, { StaticImageData } from "next/image";
 
+import useImage from "@/_shared/store/Image";
+
 interface Props {
+  id: string;
   imageUrl: string | StaticImageData;
 }
 
-export default function AlbumImage({ imageUrl }: Props) {
+export default function AlbumImage({ imageUrl, id }: Props) {
+  const { setId } = useImage();
+
+  const btnHandler = () => {
+    setId(id);
+  };
+
   return (
-    <div className="flex aspect-square w-full items-center justify-center overflow-hidden">
+    <button
+      className="flex aspect-square w-full items-center justify-center overflow-hidden"
+      onClick={btnHandler}
+    >
       <Image
         src={imageUrl}
         alt="albumImage"
         className="h-full w-full object-cover"
-        priority
+        loading="lazy"
       />
-    </div>
+    </button>
   );
-}
-
-{
-  /* <Image
-        src={imageUrl}
-        alt="albumImage"
-        className="h-full w-full"
-        priority
-      /> */
 }
