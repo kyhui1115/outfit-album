253739b4c9cc6906d9675e3f3b43bbd4c7d92bd2
kimyonghui <kyhui1115@gmail.com>
2025-07-06 19:31:49 +0900

    feat. TabBarList에서 useIsPad 훅 추가 및 클래스 이름 수정

--- src/_features/tabBar/TabBarList.tsx
+++ src/_features/tabBar/TabBarList.tsx
@@ -3,14 +3,16 @@
 import TabBarItemButton from "@/_features/tabBar/TabBarItemButton";
 import DIC from "@/_shared/dictionary";
 import { useIsMobile } from "@/_shared/hooks/useIsMobile";
+import { useIsPad } from "@/_shared/hooks/useIsPad";
 import { isPWA } from "@/_shared/utils/isPWA";
 
 export default function TabBarList() {
   const isMobile = useIsMobile();
+  const isPad = useIsPad();
 
   return (
     <div
-      className={`border-beige-300 bg-beige-100 flex h-[45px] w-full shrink-0 border-t ${isMobile ? (isPWA() ? "h-[70px]" : "h-[45px]") : "h-[50px]"}`}
+      className={`bg-beige-300 border-border-default flex h-[45px] w-full shrink-0 border-t ${isMobile || isPad ? (isPWA() ? "h-[70px]" : "h-[45px]") : "h-[50px]"}`}
     >
       {DIC.tabBarList.map(tabBar => (
         <TabBarItemButton
