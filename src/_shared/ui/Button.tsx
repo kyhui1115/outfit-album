1f4c9563c1144f924b3e741c709168265d105867
kimyonghui <kyhui1115@gmail.com>
2025-07-06 14:49:51 +0900

    style. Button 컴포넌트의 클래스 이름 순서 수정

--- src/_shared/ui/Button.tsx
+++ src/_shared/ui/Button.tsx
@@ -8,7 +8,7 @@ export default function Button({ className, children, onClick }: Props) {
   return (
     <button
       onClick={onClick}
-      className={`cursor-pointer ${className} flex items-center justify-center`}
+      className={`flex cursor-pointer items-center justify-center ${className}`}
     >
       {children}
     </button>
