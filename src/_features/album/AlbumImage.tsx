c070b29db072b3b15edd6cfd8e246578b226c8de
kimyonghui <kyhui1115@gmail.com>
2025-07-13 18:19:08 +0900

    feat. AlbumImage 컴포넌트의 버튼 클릭 핸들러 수정 및 AlbumImageList에 SelectedImageContainer 추가

--- src/_features/album/AlbumImage.tsx
+++ src/_features/album/AlbumImage.tsx
@@ -1,24 +1,45 @@
-"use client";
-
 import Image, { StaticImageData } from "next/image";
 
 import useImage from "@/_shared/store/Image";
+import useIsModal from "@/_shared/store/isModal";
 
 interface Props {
-  id: string;
   imageUrl: string | StaticImageData;
+  id: string;
 }
 
 export default function AlbumImage({ imageUrl, id }: Props) {
-  const { setId } = useImage();
+  const { setAlbumImage, setAlbumImageRect, setImage } = useImage();
+  const { setIsAlbumModal, setIsAlbumImage, isCalendarDayModal } = useIsModal();
 
-  const btnHandler = () => {
-    setId(id);
+  const btnHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const parent = e.currentTarget.parentElement;
+    if (parent) {
+      const parentRect = parent.getBoundingClientRect();
+      const rect = e.currentTarget.getBoundingClientRect();
+
+      setAlbumImageRect({
+        x: rect.left - parentRect.left,
+        y: rect.top - parentRect.top,
+        width: rect.width,
+        height: rect.height,
+      });
+    } else {
+      setAlbumImageRect(e.currentTarget.getBoundingClientRect());
+    }
+
+    if (isCalendarDayModal) {
+      setImage(imageUrl);
+      setIsAlbumModal(false);
+    } else {
+      setAlbumImage(imageUrl);
+      setIsAlbumImage(true);
+    }
   };
 
   return (
     <button
-      className="flex aspect-square w-full items-center justify-center overflow-hidden"
+      className={`aspect-square w-full items-center justify-center overflow-hidden`}
       onClick={btnHandler}
     >
       <Image
--- src/_features/album/AlbumImageList.tsx
+++ src/_features/album/AlbumImageList.tsx
@@ -1,12 +1,17 @@
+"use client";
+
 import AlbumImage from "@/_features/album/AlbumImage";
 import DIC from "@/_shared/dictionary";
 
+import SelectedImageContainer from "./selectedImage/SelectedImageContainer";
+
 export default function AlbumImageList() {
   return (
-    <div className="grid w-full grid-cols-5 content-start gap-[0.1rem]">
+    <div className="grid h-full w-full grid-cols-5 content-start gap-[0.1rem]">
       {DIC.albumImages.map(image => (
         <AlbumImage key={image.id} imageUrl={image.imageUrl} id={image.id} />
       ))}
+      <SelectedImageContainer />
     </div>
   );
 }
